60f8d066814ead82f1532cb3fef8425230bad900
Dominik Cech <dominik.cech@ibillboard.com>
2024-09-12 13:42:07 +0200

    add real nvim config

---
+++ nvim/README.md
@@ -0,0 +1,5 @@
+## First time use
+- Install packer (https://github.com/wbthomason/packer.nvim)
+- run nvim (bunch of errors will pop up, just click through)
+- run command :PackerSync (plugins should install)
+- restart neovim
---
+++ nvim/after/plugin/whichkey.lua
@@ -0,0 +1,11 @@
+require('which-key').register {
+  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  ['<leader>g'] = { name = '[G]o', _ = 'which_key_ignore' },
+  ['<leader>u'] = { name = '[U]ndo tree', _ = 'which_key_ignore' },
+  ['<leader>x'] = { name = 'Fi[x]', _ = 'which_key_ignore' },
+}
